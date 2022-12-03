4cb0ab565d097aa97c1c4759d95a25cfa85201b5
Bruno Sousa <bruno.sousa@saltpay.co>
2022-12-03 04:14:10 +0000

    Day 01 completed!

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,7 +1,35 @@
-pub fn solve_a(_input: String) -> Result<String, String> {
-    Err("Not Solved!".to_string())
+pub fn solve_a(input: String) -> Result<String, String> {
+    let res = input
+        .split("\n\n")
+        .map(|x| {
+            x.split("\n").fold(0, |acc, x| {
+                acc + match x.parse::<u32>() {
+                    Ok(val) => val,
+                    Err(_) => 0,
+                }
+            })
+        })
+        .max()
+        .unwrap();
+
+    Ok(res.to_string())
 }
 
-pub fn solve_b(_input: String) -> Result<String, String> {
-    Err("Not Solved!".to_string())
+pub fn solve_b(input: String) -> Result<String, String> {
+    let mut elves: Vec<u32> = input
+        .split("\n\n")
+        .map(|x| {
+            x.split("\n").fold(0, |acc, x| {
+                acc + match x.parse::<u32>() {
+                    Ok(val) => val,
+                    Err(_) => 0,
+                }
+            })
+        })
+        .collect();
+    elves.sort_by(|a, b| b.cmp(a));
+
+    let res: u32 = elves[0..3].into_iter().sum();
+
+    Ok(res.to_string())
 }
